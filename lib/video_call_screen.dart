68dbe412cf2dee2fd6e87cb587dbd8c1c0772fdf
AnkitCodexaHub <ankit@codexahub.com>
2025-10-18 15:06:28 +0530

    improve logic 1

--- lib/video_call_screen.dart
+++ lib/video_call_screen.dart
@@ -1,5 +1,3 @@
-// video_call_screen.dart
-
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
@@ -12,7 +10,7 @@ import 'control_bar.dart';
 class VideoCallScreen extends StatefulWidget {
   final String channelName;
   final bool isHost;
-  final String userName; // <-- The user's entered name
+  final String userName;
 
   const VideoCallScreen({
     super.key,
@@ -25,191 +23,146 @@ class VideoCallScreen extends StatefulWidget {
   State<VideoCallScreen> createState() => _VideoCallScreenState();
 }
 
-class _VideoCallScreenState extends State<VideoCallScreen> with WidgetsBindingObserver {
-  // NOTE: Replace these with your actual Agora App ID and Token
+class _VideoCallScreenState extends State<VideoCallScreen> {
   static const String appId = "2264731781464d4e8764ce1c02be1c46";
   static const String token =
-      "007eJxTYLjCwTjjUlHgIanJi4+l7F2VcZ1ZeeKR+2uOWdWJ5xj3bhRRYDAyMjMxNzY0tzA0MTNJMUm1MDczSU41TDYwSgKSJma1tp8zGgIZGdi50xgZGSAQxGdhKEktLmFgAA";
+      "007eJxTYLj8ar9N3JtuMe713pxVp7cb3lH4ae71ckkQc4vFtMBz84MUGIyMzEzMjQ3NLQxNzExSTFItzM1MklMNkw2MkoCkiZlV/OeMhkBGhk+pOUyMDBAI4rMwlKQWlzAwAAAuXx7+";
 
+  final int _localUid = Random().nextInt(1000000);
   RtcEngine? _engine;
-  bool isJoined = false;
+  bool _localUserJoined = false;
 
-  // Initialized to 0. Agora will assign the actual UID on join.
-  int _localUid = 0;
+  final List<int> _remoteUids = [];
+  final Map<int, Map<String, bool>> _remoteMuteStatus = {};
+  final Map<int, String> _userNames = {};
 
-  List<int> _remoteUids = [];
-  Map<int, Map<String, bool>> _remoteMuteStatus = {};
-  Map<int, ClientRoleType> _remoteRoles = {};
-  Map<int, String> _userNames = {};
-  Map<int, bool> _raisedHands = {};
-
-  // Active Speaker State
-  int? _activeSpeakerUid;
-
-  // Local Media State
-  bool _isMicMuted = false;
-  bool _isCameraOff = false;
+  bool _isMicMuted = true;
+  bool _isCameraOff = true;
   bool _isScreenSharing = false;
   bool _isHandRaised = false;
+
+  int? _activeSpeakerUid;
+  final Map<int, bool> _raisedHands = {};
+
+  final ClientRoleType _fixedRole = ClientRoleType.clientRoleBroadcaster;
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addObserver(this);
-
-    // REMOVED: Manual random UID generation.
-    // The name will be associated after joining the channel.
-
+    _userNames[_localUid] = widget.userName;
     _initAgora();
   }
 
-  @override
-  void dispose() {
-    _dispose();
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  Future<void> _dispose() async {
-    await _engine?.leaveChannel();
-    await _engine?.release();
-  }
-
-  Future<bool> _checkPermissions() async {
-    if (await Permission.camera.request().isDenied ||
-        await Permission.microphone.request().isDenied) {
-      return false;
-    }
-    return true;
-  }
 
   Future<void> _initAgora() async {
-    if (!await _checkPermissions()) return;
+    await [Permission.microphone, Permission.camera].request();
 
     _engine = createAgoraRtcEngine();
-    await _engine!.initialize(const RtcEngineContext(
-      appId: appId,
-    ));
+    await _engine!.initialize(const RtcEngineContext(appId: appId));
 
-    ClientRoleType clientRole =
-    widget.isHost ? ClientRoleType.clientRoleBroadcaster : ClientRoleType.clientRoleAudience;
-
-    await _engine!.setClientRole(role: clientRole);
-
-    _engine!.enableAudioVolumeIndication(
+    await _engine!.enableAudioVolumeIndication(
       interval: 200,
       smooth: 3,
       reportVad: true,
     );
 
-    _addAgoraEventHandlers();
+    _engine!.registerEventHandler(
+      RtcEngineEventHandler(
+        onJoinChannelSuccess: (connection, elapsed) =>
+            setState(() => _localUserJoined = true),
+        onUserJoined: (connection, remoteUid, elapsed) {
+          setState(() {
+            _remoteUids.add(remoteUid);
+            _remoteMuteStatus[remoteUid] = {'audio': false, 'video': false};
+            _userNames[remoteUid] = "Participant $remoteUid";
+          });
+        },
+        onUserOffline: (connection, remoteUid, reason) {
+          setState(() {
+            _remoteUids.remove(remoteUid);
+            _remoteMuteStatus.remove(remoteUid);
+            _userNames.remove(remoteUid);
+            _raisedHands.remove(remoteUid);
+          });
+        },
+        onRemoteAudioStateChanged:
+            (connection, remoteUid, state, reason, elapsed) {
+          setState(() {
+            final isMuted =
+                state == RemoteAudioState.remoteAudioStateStopped;
+            _remoteMuteStatus[remoteUid] ??= {
+              'audio': false,
+              'video': false,
+            };
+            _remoteMuteStatus[remoteUid]!['audio'] = isMuted;
+          });
+        },
+        onRemoteVideoStateChanged:
+            (connection, remoteUid, state, reason, elapsed) {
+          setState(() {
+            final isOff = state == RemoteVideoState.remoteVideoStateStopped;
+            _remoteMuteStatus[remoteUid] ??= {
+              'audio': false,
+              'video': false,
+            };
+            _remoteMuteStatus[remoteUid]!['video'] = isOff;
+          });
+        },
+        onAudioVolumeIndication:
+            (connection, speakers, totalVolume, deviceVolume) {
+          int? speakingUid;
 
-    await _engine!.enableVideo();
-    await _engine!.startPreview();
+          for (var speaker in speakers) {
+            final uid = speaker.uid == 0 ? _localUid : speaker.uid;
 
-    ChannelMediaOptions options = ChannelMediaOptions(
-      clientRoleType: clientRole,
-      channelProfile: ChannelProfileType.channelProfileCommunication,
-      publishMicrophoneTrack: !_isMicMuted,
-      publishCameraTrack: !_isCameraOff,
+            if (speaker.volume! > 5) {
+              speakingUid = uid;
+              break;
+            }
+          }
+
+          setState(() {
+            if (speakingUid != null && speakingUid != _activeSpeakerUid) {
+              _activeSpeakerUid = speakingUid;
+            } else if (speakingUid == null &&
+                _activeSpeakerUid != null &&
+                totalVolume < 5) {
+              _activeSpeakerUid = null;
+            }
+          });
+        },
+      ),
     );
 
-    // Pass _localUid (which is 0) to tell Agora to assign a unique UID.
+    await _engine!.enableVideo();
+    await _engine!.setClientRole(role: _fixedRole);
+
+    await _engine!.muteLocalAudioStream(_isMicMuted);
+    await _engine!.enableLocalVideo(!_isCameraOff);
+
     await _engine!.joinChannel(
       token: token,
       channelId: widget.channelName,
-      options: options,
       uid: _localUid,
+      options: ChannelMediaOptions(
+        channelProfile: ChannelProfileType.channelProfileLiveBroadcasting,
+        clientRoleType: _fixedRole,
+        publishCameraTrack: true,
+        publishMicrophoneTrack: true,
+        autoSubscribeVideo: true,
+        autoSubscribeAudio: true,
+      ),
     );
   }
 
-  RtcEngineEventHandler _addAgoraEventHandlers() {
-    return RtcEngineEventHandler(
-      onJoinChannelSuccess: (connection, elapsed) {
-        debugPrint("onJoinChannelSuccess: ${connection.localUid}");
-        setState(() {
-          isJoined = true;
-
-          // 1. Get the actual UID assigned by Agora
-          _localUid = connection.localUid!;
-
-          // 2. Associate the entered username with this UID
-          _userNames[_localUid] = widget.userName;
-
-          _remoteMuteStatus[_localUid] = {'audio': _isMicMuted, 'video': _isCameraOff};
-          _activeSpeakerUid = _localUid;
-        });
-      },
-
-      onUserJoined: (connection, remoteUid, elapsed) {
-        debugPrint("onUserJoined: $remoteUid");
-        setState(() {
-          _remoteUids.add(remoteUid);
-          _remoteMuteStatus[remoteUid] = {'audio': false, 'video': false};
-          // Placeholder for remote user name
-          _userNames[remoteUid] = "Participant $remoteUid";
-        });
-      },
-
-      onUserOffline: (connection, remoteUid, reason) {
-        debugPrint("onUserOffline: $remoteUid reason: $reason");
-        setState(() {
-          _remoteUids.remove(remoteUid);
-          _remoteMuteStatus.remove(remoteUid);
-          _remoteRoles.remove(remoteUid);
-          _userNames.remove(remoteUid);
-          _raisedHands.remove(remoteUid);
-          if (_activeSpeakerUid == remoteUid) {
-            _activeSpeakerUid = _localUid;
-          }
-        });
-      },
-
-      onAudioVolumeIndication: (RtcConnection connection, List<AudioVolumeInfo> speakers, int totalVolume, int deviceVolume) {
-        if (speakers.isNotEmpty) {
-          var loudestSpeaker = speakers.reduce(
-                (a, b) => (a.volume ?? 0) > (b.volume ?? 0) ? a : b,
-          );
-
-          int? speakerUid = loudestSpeaker.uid == 0 ? _localUid : loudestSpeaker.uid;
-
-          if ((loudestSpeaker.volume ?? 0) > 10 && speakerUid != _activeSpeakerUid) {
-            setState(() {
-              _activeSpeakerUid = speakerUid;
-            });
-          } else if (totalVolume < 5 && _activeSpeakerUid != _localUid) {
-            setState(() {
-              _activeSpeakerUid = _localUid;
-            });
-          }
-        }
-      },
-
-
-      onRemoteAudioStateChanged: (connection, remoteUid, state, reason, elapsed) {
-        setState(() {
-          _remoteMuteStatus[remoteUid]?['audio'] = (state == 0 || state == 1);
-        });
-      },
-
-      onRemoteVideoStateChanged: (connection, remoteUid, state, reason, elapsed) {
-        setState(() {
-          _remoteMuteStatus[remoteUid]?['video'] = (state == 0);
-        });
-      },
-    );
-  }
-
-  void _toggleMic() {
+  void _toggleMic() async {
     setState(() => _isMicMuted = !_isMicMuted);
-    _engine!.muteLocalAudioStream(_isMicMuted);
-    _remoteMuteStatus[_localUid]?['audio'] = _isMicMuted;
+    await _engine!.muteLocalAudioStream(_isMicMuted);
   }
 
-  void _toggleCamera() {
+  void _toggleCamera() async {
     setState(() => _isCameraOff = !_isCameraOff);
-    _engine!.muteLocalVideoStream(_isCameraOff);
-    _remoteMuteStatus[_localUid]?['video'] = _isCameraOff;
+    await _engine!.enableLocalVideo(!_isCameraOff);
   }
 
   void _switchCamera() {
@@ -217,22 +170,7 @@ class _VideoCallScreenState extends State<VideoCallScreen> with WidgetsBindingOb
   }
 
   void _toggleScreenShare() {
-    setState(() {
-      _isScreenSharing = !_isScreenSharing;
-    });
-  }
-
-  void _shareMeetingLink() {
-    Share.share(
-        'Join my meeting in channel ${widget.channelName}. Link/Details here...');
-  }
-
-  void _endCall() {
-    Navigator.of(context).pop();
-  }
-
-  void _refreshParticipantsList() {
-    setState(() {});
+    setState(() => _isScreenSharing = !_isScreenSharing);
   }
 
   void _toggleHand() {
@@ -247,113 +185,129 @@ class _VideoCallScreenState extends State<VideoCallScreen> with WidgetsBindingOb
     }
   }
 
-  void _toggleRemoteMic(int uid, bool isMuted) {
+  void _toggleRemoteMic(int uid, bool isMuted) async {
     if (uid == _localUid) {
       _toggleMic();
-    } else if (widget.isHost) {
-      setState(() {
-        _remoteMuteStatus[uid]?['audio'] = isMuted;
-      });
+      return;
     }
+
+    await _engine!.muteRemoteAudioStream(uid: uid, mute: isMuted);
+
+    setState(() {
+      _remoteMuteStatus[uid]!['audio'] = isMuted;
+    });
     _refreshParticipantsList();
   }
 
-  void _toggleRemoteCamera(int uid, bool isOff) {
+  void _toggleRemoteCamera(int uid, bool isOff) async {
     if (uid == _localUid) {
       _toggleCamera();
-    } else if (widget.isHost) {
-      setState(() {
-        _remoteMuteStatus[uid]?['video'] = isOff;
-      });
+      return;
     }
+
+    await _engine!.muteRemoteVideoStream(uid: uid, mute: isOff);
+
+    setState(() {
+      _remoteMuteStatus[uid]!['video'] = isOff;
+    });
     _refreshParticipantsList();
+  }
+
+  void _shareMeetingLink() {
+    Share.share('Join my live meeting: ${widget.channelName}');
+  }
+
+  void _endCall() => Navigator.of(context).pop();
+
+  void _refreshParticipantsList() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _engine?.leaveChannel();
+    _engine?.release();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    bool isHost = widget.isHost;
-    List<int> allBroadcasters = [
-      ..._remoteUids,
-      _localUid,
-    ];
+    final Map<int, ClientRoleType> allBroadcasters = {
+      _localUid: _fixedRole,
+      for (var uid in _remoteUids) uid: _fixedRole,
+    };
 
-    if (!isJoined) {
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
+    final bool isHost = widget.isHost;
 
     return Scaffold(
+      extendBodyBehindAppBar: false,
       appBar: AppBar(
+        backgroundColor: Colors.black87,
         title: Text(widget.channelName),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
+        leading: IconButton(icon: const Icon(Icons.close), onPressed: _endCall),
       ),
-      body: Stack(
+      body: Column(
         children: [
-          // Main Video Grid
-          Positioned.fill(
-            child: VideoGrid(
-              engine: _engine,
-              localUid: _localUid,
-              channelName: widget.channelName,
-              remoteUids: _remoteUids,
-              remoteRoles: _remoteRoles,
-              remoteMuteStatus: _remoteMuteStatus,
-              isLocalUserJoined: isJoined,
-              isCameraOff: _isCameraOff,
-              isMicMuted: _isMicMuted,
-              userNames: _userNames,
-              raisedHands: _raisedHands,
-              isHost: isHost,
-              activeSpeakerUid: _activeSpeakerUid,
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 15.0),
+              child: VideoGrid(
+                engine: _engine,
+                localUid: _localUid,
+                remoteUids: _remoteUids,
+                remoteRoles: allBroadcasters,
+                remoteMuteStatus: _remoteMuteStatus,
+                isLocalUserJoined: _localUserJoined,
+                isCameraOff: _isCameraOff,
+                isMicMuted: _isMicMuted,
+                channelName: widget.channelName,
+                userNames: _userNames,
+                raisedHands: _raisedHands,
+                onRoleChange: null,
+                activeSpeakerUid: _activeSpeakerUid,
+                isHost: isHost,
+              ),
             ),
           ),
-
-          // Control Bar
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: ControlBar(
-              isHost: isHost,
-              isLocalBroadcaster: isHost,
-              isMicMuted: _isMicMuted,
-              isCameraOff: _isCameraOff,
-              isScreenSharing: _isScreenSharing,
-              isHandRaised: _isHandRaised,
-              onToggleMic: _toggleMic,
-              onToggleCamera: _toggleCamera,
-              onSwitchCamera: _switchCamera,
-              onToggleScreenShare: _toggleScreenShare,
-              onShare: _shareMeetingLink,
-              onShowParticipants: () {
-                setState(() {});
-                showModalBottomSheet(
-                  context: context,
-                  isScrollControlled: true,
-                  backgroundColor: Colors.transparent,
-                  builder: (_) => ParticipantsList(
-                    localUid: _localUid,
-                    remoteUids: _remoteUids,
-                    remoteRoles: _remoteRoles,
-                    remoteMuteStatus: _remoteMuteStatus,
-                    userNames: _userNames,
-                    engine: _engine!,
-                    isHost: isHost,
-                    notifyParent: _refreshParticipantsList,
-                    isLocalMicMuted: _isMicMuted,
-                    isLocalCameraOff: _isCameraOff,
-                    onRoleChange: (_, __) {},
-                    raisedHands: _raisedHands,
-                    onToggleRemoteMic: _toggleRemoteMic,
-                    onToggleRemoteCamera: _toggleRemoteCamera,
-                  ),
-                );
-              },
-              onEndCall: _endCall,
-              onToggleHand: isHost ? null : _toggleHand,
-            ),
+          ControlBar(
+            isHost: isHost,
+            isLocalBroadcaster: true,
+            isMicMuted: _isMicMuted,
+            isCameraOff: _isCameraOff,
+            isScreenSharing: _isScreenSharing,
+            isHandRaised: _isHandRaised,
+            onToggleMic: _toggleMic,
+            onToggleCamera: _toggleCamera,
+            onSwitchCamera: _switchCamera,
+            onToggleScreenShare: _toggleScreenShare,
+            onShare: _shareMeetingLink,
+            onShowParticipants: () {
+              setState(() {});
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                backgroundColor: Colors.transparent,
+                builder: (_) => ParticipantsList(
+                  localUid: _localUid,
+                  remoteUids: _remoteUids,
+                  remoteRoles: allBroadcasters,
+                  remoteMuteStatus: _remoteMuteStatus,
+                  userNames: _userNames,
+                  engine: _engine!,
+                  isHost: isHost,
+                  notifyParent: _refreshParticipantsList,
+                  isLocalMicMuted: _isMicMuted,
+                  isLocalCameraOff: _isCameraOff,
+                  onRoleChange: (_, __) {},
+                  raisedHands: _raisedHands,
+                  onToggleRemoteMic: _toggleRemoteMic,
+                  onToggleRemoteCamera: _toggleRemoteCamera,
+                ),
+              );
+            },
+            onEndCall: _endCall,
+            onToggleHand: isHost ? null : _toggleHand,
           ),
         ],
       ),
